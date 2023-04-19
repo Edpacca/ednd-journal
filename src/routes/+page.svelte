c1a92e5ae1b8321e8e046f08d8cbd0c6ac7cf0b4
Edpacca <eddie@pace.me.uk>
2023-04-19 21:39:30 +0100

    set up basic frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script>
     import Button from "../components/common/Button.svelte";
-
 </script>
 <h1>Edpacca's Isle of Murraka</h1>
 
 <div>
-    this is where it's at
-</div>
-
-<div>
-    <Button content="player"/>
-    <Button content="visitor"/>
+    <p>Welcome traveller.</p>
+    <p>How do we find you today?</p>
+    <div class="button-menu">
+        <Button content="player"/>
+        <Button content="visitor"/>
+    </div>
 </div>
