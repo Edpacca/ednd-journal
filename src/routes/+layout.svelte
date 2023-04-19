5b3d9eea7b22d39d69fadf1ca760917c101e0322
Edpacca <eddie@pace.me.uk>
2023-04-19 21:38:42 +0100

    redo background with hardcoded svgs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,6 @@
 <script>
-    import Bar from "../components/graphics/decoration/bar.svelte";
-    import Corner from "../components/graphics/decoration/corner.svelte";
-
+    import BackgroundBorder from "../components/graphics/decoration/BackgroundBorder.svelte";
 </script>
-<svelte:head>
-    <style>
-        :root {
-            --navbar-height: 32px;
-            --footer-height: 64px;
-            
-            --default-padding: 8px;
-
-            --border-width: 2px;
-
-
-            --corner-padding: 16px;
-        }
-    </style>
-</svelte:head>
 
 <nav>
     <a href="/" class="nav-link">Home</a>
@@ -26,52 +9,35 @@
     <a href="/journal" class="nav-link">Journal</a>
 </nav>
 <main>
-    <div class="corners">
-        <Corner position="top-left" colour="burlywood"/>
-        <Corner position="top-right" colour="burlywood"/>
-        <Corner position="bottom-left" colour="burlywood"/>
-        <Corner position="bottom-right" colour="burlywood"/>
-    </div>
+    <BackgroundBorder colour="burlywood" opacity={0.4}/>
     <div class="content">
         <slot></slot>
     </div>
 </main>
 <footer>
-    <div class="footer-title">
-        Edpacca's Isle of Murraka
-    </div>
+    <h2>Edpacca's Isle of Murraka</h2>
 </footer>
 
 
 <style>
     nav {
-        background-color: burlywood;
-        color: brown;
-        font-size: 1.2rem;
+        height: var(--navbar-height);
         display: flex;
         column-gap: 16px;
         align-items: center;
         justify-content: center;
-        height: var(--navbar-height);
-        font-weight: bold;
+
+        background-color: var(--secondary);
+        color: var(--primary);
         border-bottom: var(--border-width) solid brown;
+        font-size: 1.2rem;
+        font-weight: bold;
     }
 
     main {
-        background: url("/background/water-parchment.webp");
-        top: calc(var(--navbar-height) + var(--border-width));
-        bottom: calc(var(--footer-height) + var(--border-width) + var(--navbar-height));
-        position: fixed;
-        left: 0;
-        right: 0;
-    }
-
-    .corners {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        opacity: 0.4;
-        z-index: -1;
+        position: relative;
+        flex: auto;
+        background: var(--background-url);
     }
 
     .content {
@@ -83,19 +49,10 @@
         display: flex;
         align-items: center;
         justify-content: center;
-        border-top: var(--border-width) solid brown;
-        background-color: burlywood;
-        width: 100%;
         height: var(--footer-height);
         padding: 1rem;
-        position: fixed;
-        bottom: 0;
-    }
 
-    .footer-title {
-        font-weight: bold;
-        font-size: 1.5rem;
-        color: brown
-
+        background-color: var(--secondary);
+        border-top: var(--border-width) solid var(--primary);
     }
 </style>
